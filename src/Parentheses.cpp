bbab977eecb3312de0ee07520484129545a3ae9d
Adam Mierzwiak <adam.mierzwiak@gmail.com>
2019-08-10 12:59:40 +0200

    Unpaired brackets

--- src/Parentheses.cpp
+++ src/Parentheses.cpp
@@ -1,6 +1,34 @@
+#include <map>
 #include "Parentheses.hpp"
 
-bool Parentheses::isBalanced(std::string s) {
-    return not(s[0]==')' or s[0]=='}' or s[0]==']');
 
+
+bool Parentheses::isBalanced(std::string s) {
+    if (s[0] == ')' or s[0] == '}' or s[0] == ']')
+        return false;
+
+    std::map<char, int> charCounter = {
+            {'(', 0},
+            {')', 0},
+            {'{', 0},
+            {'}', 0},
+            {'[', 0},
+            {']', 0}
+    };
+
+    for (auto letter : s) {
+        if (charCounter.find(letter) != charCounter.end()) {
+            charCounter[letter] += 1;
+
+            if (charCounter['{'] < charCounter['}'])
+                return false;
+            if (charCounter['['] < charCounter[']'])
+                return false;
+            if (charCounter['('] < charCounter[')'])
+                return false;
+        }
+    }
+
+
+    return true;
 }
