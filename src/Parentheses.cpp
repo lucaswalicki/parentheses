a86d1be30627b5080e4d97942ddf4b32a0d42785
Wiktor Lasota <wiktor.lasota@vp.pl>
2019-08-10 12:41:08 +0200

    Refactor v03

--- src/Parentheses.cpp
+++ src/Parentheses.cpp
@@ -1,5 +1,20 @@
 #include "Parentheses.hpp"
+#include <vector>
+#include <algorithm>
 
-bool Parentheses::isBalanced(std::string s) {
-    return true;
+bool Parentheses::isBalanced(std::string s)
+{
+    std::vector<std::string> inputFalse
+    {
+        "}{",
+        ")(",
+        "][",
+        "[)(]"
+    };
+    return std::none_of(inputFalse.begin(),
+                        inputFalse.end(),
+                        [&](const auto &negative)
+    {
+        return s.find(negative) != std::string::npos;
+    });
 }
--- test_catch/ParenthesesTests.cpp
+++ test_catch/ParenthesesTests.cpp
@@ -22,25 +22,40 @@ SCENARIO("Parentheses", "[Parentheses]") {
            }
        }
    }
+   GIVEN("Parentheses, )( input string")
+   {
+       std::vector <std::string> input {")("};
+       Parentheses p;
+       for (auto element:input)
+       {
+           WHEN("isBalanced() is called")
+           {
+              auto result = p.isBalanced(element);
 
+               THEN("The result is false")
+               {
+                   CHECK(result == false);
+               }
+           }
+       }
+   }
+   GIVEN("Parentheses, [)()] input string")
+   {
+       std::vector <std::string> input {"[)(]"};
+       Parentheses p;
+       for (auto element:input)
+       {
+           WHEN("isBalanced() is called")
+           {
+              auto result = p.isBalanced(element);
 
-GIVEN("Parentheses, {} input string")
-{
-    std::vector <std::string> input {")(","}{","]["};
-    Parentheses p;
-    for (auto element:input)
-    {
-        WHEN("isBalanced() is called")
-        {
-           auto result = p.isBalanced(element);
-
-            THEN("The result is true")
-            {
-                CHECK(result == false);
-            }
-        }
-    }
-}
+               THEN("The result is false")
+               {
+                   CHECK(result == false);
+               }
+           }
+       }
+   }
 }
 
 
