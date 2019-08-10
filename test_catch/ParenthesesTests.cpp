c1d700823741a33f2d442eba04e9f58beccf84ed
Wiktor Lasota <wiktor.lasota@vp.pl>
2019-08-10 11:59:16 +0200

    Test with wrong result

--- test_catch/ParenthesesTests.cpp
+++ test_catch/ParenthesesTests.cpp
@@ -22,6 +22,25 @@ SCENARIO("Parentheses", "[Parentheses]") {
            }
        }
    }
+
+
+GIVEN("Parentheses, {} input string")
+{
+    std::vector <std::string> input {")(","}{","]["};
+    Parentheses p;
+    for (auto element:input)
+    {
+        WHEN("isBalanced() is called")
+        {
+           auto result = p.isBalanced(element);
+
+            THEN("The result is true")
+            {
+                CHECK(result == false);
+            }
+        }
+    }
+}
 }
 
 
