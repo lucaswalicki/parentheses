47228969e8f0dcffbe362083b1d7a335cc0b2aa1
Wiktor Lasota <wiktor.lasota@vp.pl>
2019-08-10 11:36:41 +0200

    Test []

--- test_catch/ParenthesesTests.cpp
+++ test_catch/ParenthesesTests.cpp
@@ -29,4 +29,18 @@ SCENARIO("Parentheses", "[Parentheses]") {
            }
        }
    }
+
+   GIVEN("Parentheses, [] input string") {
+       std::string input{"[]"};
+       Parentheses p;
+
+       WHEN("isBalanced() is called") {
+           auto result = p.isBalanced(input);
+
+           THEN("The result is true") {
+               CHECK(result == true);
+           }
+       }
+   }
 }
+
