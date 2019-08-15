ad6f03a06631dc11a722bc7490332446a2307664
Daniel Tarnawski <djtarnawski@gmail.com>
2019-08-15 16:35:51 +0200

    GTest for Parentheses

--- test_gtest/ParenthesesTests.cpp
+++ test_gtest/ParenthesesTests.cpp
@@ -27,3 +27,79 @@ TEST(ParenthesesTests, givenParenthesesWhenIsBalancedAlgoIsCalledThenResultIsTru
     //THEN
     ASSERT_TRUE(result);
 }
+
+TEST(ParenthesesTests, givenGoodParenthesesWhenIsBalancedIsCalledThenResultIsTrue)
+{
+    // GIVEN
+    std::vector <std::string> input {"()","[]","{}"};
+    Parentheses p;
+    for (auto element:input)
+    {
+        // WHEN
+        auto result = p.isBalanced(element);
+
+        // THEN
+        ASSERT_EQ(true,result);
+    }
+}
+
+TEST(ParenthesesTests, givenSimpleUnbalancedParenthesesWhenIsBalancedIsCalledThenResultIsFalse)
+{
+    // GIVEN
+    std::vector <std::string> input {")(","][","}{"};
+    Parentheses p;
+    for (auto element:input)
+    {
+        // WHEN
+        auto result = p.isBalanced(element);
+
+        // THEN
+        ASSERT_EQ(false,result);
+    }
+}
+
+
+TEST(ParenthesesTests, givenRoundUnbalancedParenthesesWhenIsBalancedIsCalledThenResultIsFalse)
+{
+    // GIVEN
+    std::vector <std::string> input {"[)(]", "{)(}"};
+    Parentheses p;
+    for (auto element:input)
+    {
+        // WHEN
+        auto result = p.isBalanced(element);
+
+        // THEN
+        ASSERT_EQ(false,result);
+    }
+}
+
+TEST(ParenthesesTests, givenCurlyUnbalancedParanthesesWhenIsBalancedIsCalledThenResultIsFalse)
+{
+    // GIVEN
+    std::vector <std::string> input {"(}{)", "[}{]"};
+    Parentheses p;
+    for (auto element:input)
+    {
+        // WHEN
+        auto result = p.isBalanced(element);
+
+        // THEN
+        ASSERT_EQ(false,result);
+    }
+}
+
+TEST(ParenthesesTests, givenSquareUnbalancedParanthesesWhenIsBalancedIsCalledThenResultIsFalse)
+{
+    // GIVEN
+    std::vector <std::string> input {"{][}", "(][)"};
+    Parentheses p;
+    for (auto element:input)
+    {
+        // WHEN
+        auto result = p.isBalanced(element);
+
+        // THEN
+        ASSERT_EQ(false,result);
+    }
+}
